e738e7d1866fe13bd1a06756e215953aa77574ed
Roberto Filho <zferraz.rf@gmail.com>
2023-01-28 15:55:57 -0300

    Add and fix: Update, Delete and Search

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	f "fmt"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/spf13/cobra"
@@ -19,7 +20,7 @@ type User struct {
 
 type Company struct {
 	gorm.Model
-	Name string `gorm:"unique_index"`
+	Name string `gorm:"unique_index;"`
 }
 
 func main() {
@@ -34,7 +35,7 @@ func main() {
 
 	var rootCmd = &cobra.Command{Use: "app"}
 	var createCmd = &cobra.Command{
-		Use:   "create user and company",
+		Use:   "create",
 		Short: "Create a user and company",
 		Run: func(cmd *cobra.Command, args []string) {
 			Username, _ := cmd.Flags().GetString("username")
@@ -57,21 +58,8 @@ func main() {
 			f.Println("User and Company created with successfully.")
 		},
 	}
-	createCmd.Flags().String("username", "", "username for user")
-	createCmd.MarkFlagRequired("username")
-	createCmd.Flags().String("name", "", "name for user")
-	createCmd.MarkFlagRequired("name")
-	createCmd.Flags().Uint("age", 0, "age for user")
-	createCmd.MarkFlagRequired("age")
-	createCmd.Flags().String("nameCompany", "", "name for company")
-	createCmd.MarkFlagRequired("nameCompany")
-	createCmd.Flags().Uint("companyID", 0, "companyID for use")
-	createCmd.MarkFlagRequired("companyID")
-	rootCmd.AddCommand(createCmd)
-	rootCmd.Execute()
-
 	var updateCmd = &cobra.Command{
-		Use:   "update user and company",
+		Use:   "update",
 		Short: "Update a user and company",
 		Run: func(cmd *cobra.Command, args []string) {
 			Username, _ := cmd.Flags().GetString("username")
@@ -102,22 +90,8 @@ func main() {
 			f.Println("User and Company updated with successfully.")
 		},
 	}
-
-	updateCmd.Flags().String("username", "", "update username for user")
-	updateCmd.MarkFlagRequired("username")
-	updateCmd.Flags().String("name", "", "update name for user")
-	updateCmd.MarkFlagRequired("name")
-	updateCmd.Flags().Uint("age", 0, "update age for user")
-	updateCmd.MarkFlagRequired("age")
-	updateCmd.Flags().String("nameCompany", "", "update name for company")
-	updateCmd.MarkFlagRequired("nameCompany")
-	updateCmd.Flags().Uint("companyID", 0, "update companyID for use")
-	updateCmd.MarkFlagRequired("companyID")
-	rootCmd.AddCommand(updateCmd)
-	rootCmd.Execute()
-
 	var deleteCmd = &cobra.Command{
-		Use:   "Delete user and company",
+		Use:   "delete",
 		Short: "Delete a user and company",
 		Run: func(cmd *cobra.Command, args []string) {
 			Username, _ := cmd.Flags().GetString("username")
@@ -141,17 +115,9 @@ func main() {
 			f.Println("User and Company deleted with successfully.")
 		},
 	}
-
-	deleteCmd.Flags().String("username", "", "delete username for user")
-	deleteCmd.MarkFlagRequired("username")
-	deleteCmd.Flags().String("nameCompany", "", "delete name for company")
-	deleteCmd.MarkFlagRequired("nameCompany")
-	rootCmd.AddCommand(deleteCmd)
-	rootCmd.Execute()
-
 	var listCmd = &cobra.Command{
-		Use:   "Search user and company",
-		Short: "Search a user and company",
+		Use:   "search",
+		Short: "search a user and company",
 		Run: func(cmd *cobra.Command, args []string) {
 			Username, _ := cmd.Flags().GetString("username")
 			nameCompany, _ := cmd.Flags().GetString("nameCompany")
@@ -163,10 +129,136 @@ func main() {
 			f.Println("Company:", company)
 		},
 	}
+	createCmd.Flags().String("username", "", "username for user")
+	createCmd.MarkFlagRequired("username")
+	createCmd.Flags().String("name", "", "name for user")
+	createCmd.MarkFlagRequired("name")
+	createCmd.Flags().Uint("age", 0, "age for user")
+	createCmd.MarkFlagRequired("age")
+	createCmd.Flags().String("nameCompany", "", "name for company")
+	createCmd.MarkFlagRequired("nameCompany")
+	createCmd.Flags().Uint("companyID", 0, "companyID for use")
+	createCmd.MarkFlagRequired("companyID")
+
+	updateCmd.Flags().String("username", "", "update username for user")
+	updateCmd.MarkFlagRequired("username")
+	updateCmd.Flags().String("name", "", "update name for user")
+	updateCmd.MarkFlagRequired("name")
+	updateCmd.Flags().Uint("age", 0, "update age for user")
+	updateCmd.MarkFlagRequired("age")
+	updateCmd.Flags().String("nameCompany", "", "update name for company")
+	updateCmd.MarkFlagRequired("nameCompany")
+	updateCmd.Flags().Uint("companyID", 0, "update companyID for use")
+	updateCmd.MarkFlagRequired("companyID")
+
+	deleteCmd.Flags().String("username", "", "delete username for user")
+	deleteCmd.MarkFlagRequired("username")
+	deleteCmd.Flags().String("nameCompany", "", "delete name for company")
+	deleteCmd.MarkFlagRequired("nameCompany")
+
 	listCmd.Flags().String("username", "", "search username for user")
 	listCmd.MarkFlagRequired("username")
 	listCmd.Flags().String("nameCompany", "", "search name for company")
 	listCmd.MarkFlagRequired("nameCompany")
-	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(createCmd, updateCmd, deleteCmd, listCmd)
 	rootCmd.Execute()
+
+	// var updateCmd = &cobra.Command{
+	// 	Use:   "update",
+	// 	Short: "Update a user and company",
+	// 	Run: func(cmd *cobra.Command, args []string) {
+	// 		Username, _ := cmd.Flags().GetString("username")
+	// 		Name, _ := cmd.Flags().GetString("name")
+	// 		Age, _ := cmd.Flags().GetUint("age")
+	// 		nameCompany, _ := cmd.Flags().GetString("nameCompany")
+	// 		CompanyID, _ := cmd.Flags().GetUint("companyID")
+	// 		tx := db.Begin()
+	// 		var user User
+	// 		db.Where("username = ?", Username).First(&user)
+	// 		user.Name = Name
+	// 		user.Age = Age
+	// 		user.CompanyID = CompanyID
+	// 		if err := tx.Save(&user).Error; err != nil {
+	// 			f.Println("Error updating user:", err)
+	// 			tx.Rollback()
+	// 			return
+	// 		}
+	// 		var company Company
+	// 		db.Where("name = ?", nameCompany).First(&company)
+	// 		company.Name = nameCompany
+	// 		if err := tx.Save(&company).Error; err != nil {
+	// 			f.Println("Error updating company:", err)
+	// 			tx.Rollback()
+	// 			return
+	// 		}
+	// 		tx.Commit()
+	// 		f.Println("User and Company updated with successfully.")
+	// 	},
+
+	// updateCmd.Flags().String("username", "", "update username for user")
+	// updateCmd.MarkFlagRequired("username")
+	// updateCmd.Flags().String("name", "", "update name for user")
+	// updateCmd.MarkFlagRequired("name")
+	// updateCmd.Flags().Uint("age", 0, "update age for user")
+	// updateCmd.MarkFlagRequired("age")
+	// updateCmd.Flags().String("nameCompany", "", "update name for company")
+	// updateCmd.MarkFlagRequired("nameCompany")
+	// updateCmd.Flags().Uint("companyID", 0, "update companyID for use")
+	// updateCmd.MarkFlagRequired("companyID")
+	// rootCmd.AddCommand(updateCmd)
+	// rootCmd.Execute()
+
+	// var deleteCmd = &cobra.Command{
+	// 	Use:   "delete",
+	// 	Short: "Delete a user and company",
+	// 	Run: func(cmd *cobra.Command, args []string) {
+	// 		Username, _ := cmd.Flags().GetString("username")
+	// 		nameCompany, _ := cmd.Flags().GetString("nameCompany")
+	// 		tx := db.Begin()
+	// 		var user User
+	// 		db.Where("username = ?", Username).First(&user)
+	// 		if err := tx.Delete(&user).Error; err != nil {
+	// 			f.Println("Error deleting user:", err)
+	// 			tx.Rollback()
+	// 			return
+	// 		}
+	// 		var company Company
+	// 		db.Where("name = ?", nameCompany).First(&company)
+	// 		if err := tx.Delete(&company).Error; err != nil {
+	// 			f.Println("Error deleting company:", err)
+	// 			tx.Rollback()
+	// 			return
+	// 		}
+	// 		tx.Commit()
+	// 		f.Println("User and Company deleted with successfully.")
+	// 	},
+	// }
+
+	// deleteCmd.Flags().String("username", "", "delete username for user")
+	// deleteCmd.MarkFlagRequired("username")
+	// deleteCmd.Flags().String("nameCompany", "", "delete name for company")
+	// deleteCmd.MarkFlagRequired("nameCompany")
+	// rootCmd.AddCommand(deleteCmd)
+	// rootCmd.Execute()
+
+	// var listCmd = &cobra.Command{
+	// 	Use:   "search",
+	// 	Short: "search a user and company",
+	// 	Run: func(cmd *cobra.Command, args []string) {
+	// 		Username, _ := cmd.Flags().GetString("username")
+	// 		nameCompany, _ := cmd.Flags().GetString("nameCompany")
+	// 		var user User
+	// 		db.Where("username = ?", Username).First(&user)
+	// 		f.Println("User:", user)
+	// 		var company Company
+	// 		db.Where("name = ?", nameCompany).First(&company)
+	// 		f.Println("Company:", company)
+	// 	},
+	// }
+	// listCmd.Flags().String("username", "", "search username for user")
+	// listCmd.MarkFlagRequired("username")
+	// listCmd.Flags().String("nameCompany", "", "search name for company")
+	// listCmd.MarkFlagRequired("nameCompany")
+	// rootCmd.AddCommand(listCmd)
+	// rootCmd.Execute()
 }
